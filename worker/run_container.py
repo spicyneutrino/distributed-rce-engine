28ca5fa9b472f5550b380abeef46b257e871dd5f
spicyneutrino <107169289+spicyneutrino@users.noreply.github.com>
2025-12-27 01:51:42 -0600

    Add Seccomp in config

--- worker/run_container.py
+++ worker/run_container.py
@@ -1,48 +1,48 @@
 import subprocess
 import os
-import tempfile
+
 
 def run_code_in_container(code_content: bytes) -> str:
-    with tempfile.NamedTemporaryFile(suffix=".py", delete=False, mode= 'wb', dir=os.getcwd()) as tmp:
-        tmp.write(code_content)
-        tmp_path = tmp.name
-        
+    # Path only for Seccomp
+    host_path = os.getenv("HOST_PROJECT_PATH")
+    seccomp_profile_path = os.path.join(host_path, "seccomp_profile.json")
+
     try:
-        # Podman Container
-        # --rm: Delete container after run
-        # --network none: NO INTERNET ACCESS (Security)
-        # -v: Mount the temp file into the container at /app/script.py
-        # python:3.9-slim: The image to run
+        # Pass the code directly to Python via "Standard Input" (stdin).
+        
         cmd = [
             "podman", "run", "--rm",
+            "-i",              # Interactive (Allows reading input)
             "--network", "none",
-            "--memory", "128m",  # Limit RAM to prevent crashes
-            "--cpus", "0.5",     # Limit CPU
-            "-v", f"{tmp_path}:/app/script.py:ro,Z",
-            # "docker.io/library/python:3.9-slim",
+            "--memory", "128m",
+            "--cpus", "0.5",
+            "--pids-limit", "64",
+            "--cap-drop=ALL",
+            # Mount the Seccomp profile because it's a config file, not user data
+            f"--security-opt=seccomp={seccomp_profile_path}",
+            
             "localhost/rce-datascience",
-            "python", "/app/script.py"
+            "python", "-"      # The "-" tells Python to read code from stdin
         ]
         
         print(f"DEBUG: Executing {' '.join(cmd)}")
         
+        # Pass the code_content directly into 'input='
         result = subprocess.run(
             cmd,
+            input=code_content.decode('utf-8'), # Pipe the code string here
             capture_output=True,
             text=True,
-            timeout=10 # Kill if it runs longer than 10s
+            timeout=10
         )
         
         if result.returncode == 0:
             return result.stdout
         else:
+            # If Seccomp blocks it, we can see it here
             return f"Error (Exit Code {result.returncode}):\n{result.stderr}"
     
     except subprocess.TimeoutExpired:
         return "Error: Execution timed out."
     except Exception as e:
         return f"System Error: {str(e)}"
-    finally:
-        # Cleanup temp file on host
-        if os.path.exists(tmp_path):
-            os.remove(tmp_path)
